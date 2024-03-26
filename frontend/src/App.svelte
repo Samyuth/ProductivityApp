6eac7a82e6e23b498a4f36e023ba24612f203ef2
Samyuth <samyuth.sagi@gmail.com>
2024-03-26 12:56:17 -0400

    Tab and save implemented in textarea

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,30 +2,72 @@
 	import { onMount } from 'svelte';
 
 	const endpoint = "http://localhost:5000/";
-	let api_text;
 
+	// State
+	let planner_text;
+	let textarea_elem;
+
+	let tab_selected = 0;
+	let tab = false;
+
+	// Load test.txt on mount
 	onMount(async () => {
 		const response = await fetch(endpoint);
 		const data = await response.json();
-		api_text = data["text"];
+		planner_text = data["text"];
 	});
 
+	// Save functionality
 	const update_planner = () => {
 		fetch(endpoint, {
 			method: "POST",
 			body: JSON.stringify({
-				data: api_text
+				data: planner_text
 			}),
 			headers: {
 				"Content-type": "application/json; charset=UTF-8"
 			}
 		});
 	}
+
+	// Key down funcitonality
+	const keydown = e => {
+		tabset();
+
+		// save
+		if (e.ctrlKey && e.keyCode == 83) {
+			e.preventDefault();
+			update_planner();
+		} /* tab */ else if (e.keyCode == 9) {
+			e.preventDefault();
+			const {selectionStart, selectionEnd} = textarea_elem;
+
+			tab_selected = selectionStart + 1;
+			tab = true;
+
+			planner_text = planner_text.substring(0, selectionStart) 
+				+ '\t' + planner_text.substring(selectionEnd)
+
+			textarea_elem.setSelectionRange(tab_selected, tab_selected)
+		}
+	}
+
+	const tabset = () => {
+		if (tab) {
+			textarea_elem.setSelectionRange(tab_selected, tab_selected)
+			tab = false;
+		}
+	};
 </script>
 
 <main>
 	<div class="container">
-		<textarea class="scratch-pad" bind:value={api_text}></textarea>
+		<textarea
+			class="scratch-pad"
+			bind:this={textarea_elem}
+			bind:value={planner_text}
+			on:keydown={keydown}
+			on:keyup={() => {tabset()}}/>
 		<button class="lock-in-btn" on:click={update_planner}>Lock in</button>
 	</div>
 </main>
